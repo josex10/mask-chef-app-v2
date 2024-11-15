68bed6834f4c7eec8e2c22f9d7bc6db1acb5079d
jose.badilla <jose.badilla@First-Factory-Inc-MacBookPro-P3997649J9.local>
2024-11-15 14:24:20 -0600

    add filters for dashboard

--- src/components/private/admin/dashboard/DashboardDateButton.tsx
+++ src/components/private/admin/dashboard/DashboardDateButton.tsx
@@ -1,13 +1,84 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { EDashboardButtonFilterType } from "@/utils/enums/dashboard/EDashboardButtonFilterType";
+import { addMonths, fromUnixTime, getUnixTime, subMonths } from "date-fns";
+import { ChevronLeft, ChevronRight, PiIcon } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
-const DashboardDateButton = () => {
+const isDisableButton = (
+  date: string | null,
+  type: EDashboardButtonFilterType
+) => {
+  if (!date && type === EDashboardButtonFilterType.NEXT) {
+    return true;
+  }
+
+  if (date && type === EDashboardButtonFilterType.NEXT) {
+    const actualDate = new Date();
+    const dateToCompare = fromUnixTime(Number(date));
+    if (
+      actualDate.getMonth() === dateToCompare.getMonth() &&
+      actualDate.getFullYear() === dateToCompare.getFullYear()
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const modifyDateUnix = (unixDate: string | null, type: EDashboardButtonFilterType) => {
+  let adjustedDate: Date = new Date();
+  if (unixDate === null) {
+    return getUnixTime(adjustedDate).toString();
+  }
+  const date = fromUnixTime(Number(unixDate));
+  if (type === EDashboardButtonFilterType.NEXT) {
+    adjustedDate = addMonths(date, 1);
+  } else if (type === EDashboardButtonFilterType.PREV) {
+    adjustedDate = subMonths(date, 1);
+  }
+  return getUnixTime(adjustedDate).toString();
+};
+
+const DashboardDateButton = ({
+  type,
+}: {
+  type: EDashboardButtonFilterType;
+}) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const dateParam = searchParams.get("date");
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
   const handleClick = async (e: any) => {
-    console.log("click");
+    const date = modifyDateUnix(dateParam, type);
+    router.push(
+      pathname +
+        "?" +
+        createQueryString("date", date)
+    );
   };
+
   return (
     <form action={handleClick}>
       <input type="hidden" value={""} name="color" />
-      <button type="submit">Prev</button>
+      <Button disabled={isDisableButton(dateParam, type)} type="submit" variant="ghost">
+        
+        
+        {type === EDashboardButtonFilterType.NEXT ? <ChevronRight height={24} />: <ChevronLeft height={24} />}
+      </Button>
     </form>
   );
 };
--- src/components/private/admin/navbar/AdminNavbar.tsx
+++ src/components/private/admin/navbar/AdminNavbar.tsx
@@ -21,7 +21,7 @@ const AdminNavbarComponent = ({ cookie }: TAdminNavbarComponent) => {
   return (
     <header className="flex h-[8vh] items-center gap-4 border-b bg-muted px-4 sticky top-0 z-50">
       <AdminNavbarSheetComponent />
-      <div className="flex-1"></div>
+      <div className="flex-1"><span className="hidden">Text Title</span></div>
       <div className="flex flex-row gap-4">
         <AdminNavbarRestName />
         <SignedIn>
